20442dc52ddfb1595ce222e28105d7da67b50bf5
= <alex.patey@btinternet.com>
2022-08-10 20:05:48 +0100

    Added support for Google Vision API

--- AlexPWeb/Controllers/HomeController.cs
+++ AlexPWeb/Controllers/HomeController.cs
@@ -1,6 +1,10 @@
 ﻿using AlexPWeb.Models;
+using Google.Apis.Auth.OAuth2;
+using Google.Cloud.Storage.V1;
+using Google.Cloud.Vision.V1;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using System.Drawing;
 
 namespace AlexPWeb.Controllers
 {
@@ -15,6 +19,34 @@ namespace AlexPWeb.Controllers
 
         public IActionResult Index()
         {
+            //Google.Cloud.Vision.V1.Image image1 = Google.Cloud.Vision.V1.Image.FromFile("D:\\Working Folder\\AlexPWeb\\AlexPWeb\\TestImages\\HandwrittenImage.jpg");
+
+            //ImageAnnotatorClientBuilder builder = new ImageAnnotatorClientBuilder
+            //{
+            //    CredentialsPath = "D:\\Working Folder\\AlexPWeb\\AlexPWeb\\GoogleAPIKey\\alexpweb-4891e60e4e6a.json"
+            //};
+
+            //ImageAnnotatorClient client = builder.Build();
+
+            //TextAnnotation text = client.DetectDocumentText(image1);
+            //Console.WriteLine($"Text: {text.Text}");
+            //foreach (var page in text.Pages)
+            //{
+            //    foreach (var block in page.Blocks)
+            //    {
+            //        string box = string.Join(" - ", block.BoundingBox.Vertices.Select(v => $"({v.X}, {v.Y})"));
+            //        Console.WriteLine($"Block {block.BlockType} at {box}");
+            //        foreach (var paragraph in block.Paragraphs)
+            //        {
+            //            box = string.Join(" - ", paragraph.BoundingBox.Vertices.Select(v => $"({v.X}, {v.Y})"));
+            //            Console.WriteLine($"  Paragraph at {box}");
+            //            foreach (var word in paragraph.Words)
+            //            {
+            //                Console.WriteLine($"    Word: {string.Join("", word.Symbols.Select(s => s.Text))}");
+            //            }
+            //        }
+            //    }
+            //}
             return View();
         }
 
