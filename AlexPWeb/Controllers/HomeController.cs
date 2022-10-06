7ae6a6b12ee493b1ab9e84a74d7cf28aabc660dd
= <alex.patey@btinternet.com>
2022-10-06 18:51:08 +0100

    get image from local files and extract image text

--- AlexPWeb/Controllers/HomeController.cs
+++ AlexPWeb/Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using Google.Cloud.Vision.V1;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 using System.Drawing;
+using System.Text.RegularExpressions;
 
 namespace AlexPWeb.Controllers
 {
@@ -19,35 +20,48 @@ namespace AlexPWeb.Controllers
 
         public IActionResult Index()
         {
-            //Google.Cloud.Vision.V1.Image image1 = Google.Cloud.Vision.V1.Image.FromFile("D:\\Working Folder\\AlexPWeb\\AlexPWeb\\TestImages\\HandwrittenImage.jpg");
-
-            //ImageAnnotatorClientBuilder builder = new ImageAnnotatorClientBuilder
-            //{
-            //    CredentialsPath = "D:\\Working Folder\\AlexPWeb\\AlexPWeb\\GoogleAPIKey\\alexpweb-4891e60e4e6a.json"
-            //};
-
-            //ImageAnnotatorClient client = builder.Build();
-
-            //TextAnnotation text = client.DetectDocumentText(image1);
-            //Console.WriteLine($"Text: {text.Text}");
-            //foreach (var page in text.Pages)
-            //{
-            //    foreach (var block in page.Blocks)
-            //    {
-            //        string box = string.Join(" - ", block.BoundingBox.Vertices.Select(v => $"({v.X}, {v.Y})"));
-            //        Console.WriteLine($"Block {block.BlockType} at {box}");
-            //        foreach (var paragraph in block.Paragraphs)
-            //        {
-            //            box = string.Join(" - ", paragraph.BoundingBox.Vertices.Select(v => $"({v.X}, {v.Y})"));
-            //            Console.WriteLine($"  Paragraph at {box}");
-            //            foreach (var word in paragraph.Words)
-            //            {
-            //                Console.WriteLine($"    Word: {string.Join("", word.Symbols.Select(s => s.Text))}");
-            //            }
-            //        }
-            //    }
-            //}
+            
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult ConvertImgToText(string imgSrc)
+        {
+            imgSrc = imgSrc.Replace("data:image/jpeg;base64,", "");
+            imgSrc = imgSrc.Replace("data:image/png;base64,", "");
+
+            byte[] bytes = Convert.FromBase64String(imgSrc);
+
+            Google.Cloud.Vision.V1.Image image1 = Google.Cloud.Vision.V1.Image.FromBytes(bytes);
+
+            ImageAnnotatorClientBuilder builder = new ImageAnnotatorClientBuilder
+            {
+                CredentialsPath = "D:\\Working Folder\\AlexPWeb\\AlexPWeb\\GoogleAPIKey\\alexpweb-4891e60e4e6a.json"
+            };
+
+            ImageAnnotatorClient client = builder.Build();
+
+            TextAnnotation text = client.DetectDocumentText(image1);
+            Console.WriteLine($"Text: {text.Text}");
+            foreach (var page in text.Pages)
+            {
+                foreach (var block in page.Blocks)
+                {
+                    string box = string.Join(" - ", block.BoundingBox.Vertices.Select(v => $"({v.X}, {v.Y})"));
+                    Console.WriteLine($"Block {block.BlockType} at {box}");
+                    foreach (var paragraph in block.Paragraphs)
+                    {
+                        box = string.Join(" - ", paragraph.BoundingBox.Vertices.Select(v => $"({v.X}, {v.Y})"));
+                        Console.WriteLine($"  Paragraph at {box}");
+                        foreach (var word in paragraph.Words)
+                        {
+                            Console.WriteLine($"    Word: {string.Join("", word.Symbols.Select(s => s.Text))}");
+                        }
+                    }
+                }
+            }
+
+            return Json(new {success = true, text = text.Text});
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
---
+++ AlexPWeb/wwwroot/js/home.js
@@ -0,0 +1,34 @@
+﻿$('#f').on('change', function (ev) {
+    var f = ev.target.files[0];
+    var fr = new FileReader();
+
+    fr.onload = function (ev2) {
+        console.dir(ev2);
+        $('#i').attr('src', ev2.target.result);
+    };
+
+    fr.readAsDataURL(f);
+
+    document.getElementById('convertImgToText').hidden = false;
+});
+
+$('#convertImgToText').click(function () {
+
+    var imgSrc = document.getElementById("i").getAttribute("src");
+
+    $.ajax({
+        type: "POST",
+        url: "Home/ConvertImgToText",
+        async: true,
+        data: {
+            imgSrc: imgSrc,
+        },
+        success: function (result) {
+            console.log(result.text);
+        },
+        error: function (req, status, error) {
+            alert("error with convert img ajax call!")
+        }
+    });
+
+});
