cff81444805d778d2ae4c4aa44980ff6f08e0f99
= <alex.patey@btinternet.com>
2022-10-06 20:25:34 +0100

    displays extracted text in text area; style improvements

--- AlexPWeb/wwwroot/js/home.js
+++ AlexPWeb/wwwroot/js/home.js
@@ -1,20 +1,20 @@
-﻿$('#f').on('change', function (ev) {
+﻿$('#fileUpload').on('change', function (ev) {
     var f = ev.target.files[0];
     var fr = new FileReader();
 
     fr.onload = function (ev2) {
         console.dir(ev2);
-        $('#i').attr('src', ev2.target.result);
+        $('img').attr('src', ev2.target.result);
     };
 
     fr.readAsDataURL(f);
-
+    document.getElementById('imgPreviewText').hidden = false;
     document.getElementById('convertImgToText').hidden = false;
 });
 
 $('#convertImgToText').click(function () {
 
-    var imgSrc = document.getElementById("i").getAttribute("src");
+    var imgSrc = document.getElementById('img').getAttribute('src');
 
     $.ajax({
         type: "POST",
@@ -25,6 +25,9 @@ $('#convertImgToText').click(function () {
         },
         success: function (result) {
             console.log(result.text);
+            document.getElementById('imgText').innerHTML = result.text;
+            document.getElementById('imgText').hidden = false;
+            document.getElementById('saveTxtFileBtn').hidden = false;
         },
         error: function (req, status, error) {
             alert("error with convert img ajax call!")
